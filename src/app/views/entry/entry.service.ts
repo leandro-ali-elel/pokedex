635ba5b5b42a1fb25fa179e0b3639f17a568038f
Ali <ali@cuoma.com>
2022-07-28 00:47:58 -0300

    reused pokemonservice

--- src/app/views/entry/entry.service.ts
+++ src/app/views/entry/entry.service.ts
@@ -3,20 +3,13 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {Trainer} from 'src/app/core/models/trainer';
-import { environment } from 'src/environments/environment';
+import {PokemonService} from 'src/app/core/services/pokemon.service';
+import {environment} from 'src/environments/environment';
 @Injectable()
 export class EntryService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private pokemonService: PokemonService) {}
 
   public login(username: string): Observable<Trainer> {
-    return this.httpClient.get<Trainer[]>(`${environment.trainersAPIURL}`).pipe(
-      map(trainers => {
-        const selectedTrainer = trainers.find(trainer => trainer.name === username);
-        if (!selectedTrainer) {
-          throw new HttpErrorResponse({error: 403, statusText: `cannot find username in database`});
-        }
-        return selectedTrainer;
-      })
-    );
+    return this.pokemonService.findTrainer(username);
   }
 }

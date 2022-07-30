c9f5110b4c9a408c37dba5e548d28dde054f72c8
Ali <ali@cuoma.com>
2022-07-30 13:23:40 -0300

    created guards for login

--- src/app/core/guards/login.guard.ts
+++ src/app/core/guards/login.guard.ts
@@ -2,20 +2,23 @@ import {Injectable} from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import {Observable} from 'rxjs';
+import {hasToken} from 'src/app/utils/authentication-utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginGuard implements CanActivate {
-  canActivate(
+  constructor(private router: Router) {}
+  public canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     // there I would check for an access token normally
-    return true;
+    return hasToken() ? this.router.createUrlTree(['/dashboard']) : true;
   }
 }
--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -2,21 +2,23 @@ import {Injectable} from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import {Observable} from 'rxjs';
+import {hasToken} from 'src/app/utils/authentication-utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SessionGuard implements CanActivate {
-  canActivate(
+  constructor(private router: Router) {}
+  public canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // there I would check for the ausence of an access token
-
-    return true;
+    // there I would check for an access token normally
+    return hasToken() ? true : this.router.createUrlTree(['/entry']);
   }
 }

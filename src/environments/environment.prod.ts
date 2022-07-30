518436e1373fb85d2a6de33f57f6bbb35913a49d
Ali <ali@cuoma.com>
2022-07-30 02:29:40 -0300

    fixed wrong url for trainer data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  trainersAPIURL: '/app/core/constants/mock-pokemons.json',
+  trainersAPIURL: 'assets/data.json',
   pokeAPIURL: 'https://beta.pokeapi.co/graphql/v1beta',
   criesSoundbankURL: 'https://pokemoncries.com/cries-old/',
   dadJokesAPI: 'https://icanhazdadjoke.com/graphql'

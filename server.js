91f55fb15833a18b7a8e241c877b2512502a6158
Ali <ali@cuoma.com>
2022-07-30 20:55:25 -0300

    disabled gzip

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const compression = require('compression')
-app.use(compression())
+// const compression = require('compression')
+// app.use(compression())
 app.use(express.static(__dirname + '/dist/pokedex'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +

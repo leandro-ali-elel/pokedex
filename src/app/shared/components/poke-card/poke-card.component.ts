39d0ad5e60f8b382f83696cd1a647a767b55dc50
Ali <ali@cuoma.com>
2022-07-30 13:57:52 -0300

    improved responsive for all pages

--- src/app/shared/components/poke-card/poke-card.component.ts
+++ src/app/shared/components/poke-card/poke-card.component.ts
@@ -1,13 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {Pokemon} from 'src/app/core/models/interfaces/pokemon';
-import {PokemonImages} from 'src/app/core/models/interfaces/pokemon-images';
-import {extractImageFromSprite} from 'src/app/utils/pokemon-utils';
 
 @Component({
   selector: 'poke-poke-card',
@@ -15,18 +7,6 @@ import {extractImageFromSprite} from 'src/app/utils/pokemon-utils';
   styleUrls: ['./poke-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokeCardComponent implements OnChanges {
+export class PokeCardComponent {
   @Input() public pokemon!: Pokemon;
-  protected pokemonImg?: PokemonImages;
-
-  public ngOnChanges(simpleChanges: SimpleChanges): void {
-    const pokemon: Pokemon | undefined = simpleChanges.pokemon?.currentValue;
-    if (pokemon && pokemon.pokemon_v2_pokemonsprites.length > 0) {
-      this.pokemonImg = extractImageFromSprite(pokemon);
-    }
-  }
-}
-
-export interface PokeCardConfig {
-  mode: 'vertical' | 'horizontal';
 }
--- src/app/shared/components/poke-card/poke-card.module.ts
+++ src/app/shared/components/poke-card/poke-card.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import { ExtractPokemonModule } from '../../pipes/extract-pokemon.module';
 import {PokeCardComponent} from './poke-card.component';
 
 @NgModule({
   declarations: [PokeCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ExtractPokemonModule],
   exports: [PokeCardComponent],
 })
 export class PokeCardModule {}

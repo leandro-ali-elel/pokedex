2d47c7329528c44c25acc9b685b1dc88899aa2d1
Ali <ali@cuoma.com>
2022-07-30 23:13:18 -0300

    shame server.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const compression = require('compression')
-app.use(compression())
-app.use(express.static(__dirname + '/dist/pokedex'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/pokedex/index.html'));
+    res.sendFile('index.html', { root: 'dist/pokedex/' }
+    );
 });
 app.listen(process.env.PORT || 8080);

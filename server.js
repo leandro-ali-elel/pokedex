de5308c743c3c4bfb0afaa8715fdc8a1d6084855
Ali <ali@cuoma.com>
2022-07-30 23:22:33 -0300

    returned to path approach

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const compression = require('compression')
+app.use(compression())
+app.use(express.static(__dirname + '/dist/pokedex'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/pokedex/' }
-    );
+    res.sendFile(path.join(__dirname +
+        '/dist/pokedex/index.html'));
 });
 app.listen(process.env.PORT || 8080);

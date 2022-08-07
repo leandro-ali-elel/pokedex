615b8fccfde709731b1c1af95c3a12d0f098740e
Ali <ali@cuoma.com>
2022-08-06 23:58:17 -0300

    set up obs for state machine

--- src/app/views/dashboard/fight/fight.component.ts
+++ src/app/views/dashboard/fight/fight.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { Pokemon } from 'src/app/core/models/interfaces/pokemon';
+import {combineLatest, forkJoin, merge, Observable, Subject} from 'rxjs';
+import {concatMap, filter, first, mergeMap, scan, switchMap, tap} from 'rxjs/operators';
+import {Pokemon} from 'src/app/core/models/interfaces/pokemon';
 import {PokemonFighter} from 'src/app/core/models/interfaces/pokemon-fighter';
 import {FightFacade} from './fight.facade';
 
@@ -11,13 +11,24 @@ import {FightFacade} from './fight.facade';
   styleUrls: ['./fight.component.scss'],
 })
 export class FightComponent implements OnInit {
-  myPokemon$!: Observable<Pokemon>;
-  enemyPokemon$!: Observable<Pokemon>;
-
+  protected stateMachine$!: Observable<any>;
+  private playerPokemon$!: Observable<Pokemon>;
+  private enemyPokemon$!: Observable<Pokemon>;
+  private attack$ = new Subject();
   constructor(private fightFacade: FightFacade) {}
 
   ngOnInit(): void {
-    this.myPokemon$ = this.fightFacade.getMyPokemonFighterStats().pipe(tap(console.log));
-    this.enemyPokemon$ = this.fightFacade.getEnemyPokemonFighterStats().pipe(tap(console.log));
+    this.playerPokemon$ = this.fightFacade.getMyPokemonFighterStats().pipe(first());
+    this.enemyPokemon$ = this.fightFacade.getEnemyPokemonFighterStats().pipe(first());
+    this.stateMachine$ = forkJoin([this.playerPokemon$, this.enemyPokemon$]).pipe();
+  }
+
+  protected attack(): void {
+    // this.stateMachine$.next({turn: 'enemy'});
+    this.attack$.next();
+  }
+
+  protected startBattle(): void {
+    // this.stateMachine$.next({turn: 'player'});
   }
 }
